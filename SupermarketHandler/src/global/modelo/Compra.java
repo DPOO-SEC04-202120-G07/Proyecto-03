29f601a6f00eb6b93b4e903eafb69b66de862c83
Camilo Morillo <c.morilloq@uniandes.edu.co>
2021-10-08 10:12:14 -0500

    Implementacion compra

--- SupermarketHandler/src/global/modelo/Compra.java
+++ SupermarketHandler/src/global/modelo/Compra.java
@@ -1,5 +1,23 @@
 package global.modelo;
 
 public class Compra {
-
+	int precioTotal;
+	int puntosCompra;
+	String resumenPedido;
+	
+	public Compra() {
+		precioTotal=0;
+		puntosCompra=0;
+		resumenPedido="";
+	}
+	
+	public void agregarProductoCompra(Producto producto) {
+		precioTotal+=producto.getPrecio();
+		puntosCompra+=producto.getPrecio()/2;
+		resumenPedido+=producto.getNombre()+" ("+producto.getMarca()+") -- Precio: "+producto.getPrecio()+"\n";
+	}
+	
+	public void cerrarCompra() {
+		resumenPedido+="Precio Total: "+precioTotal+" -- Puntos totales obtenidos: "+puntosCompra;
+	}
 }
--- SupermarketHandler/src/global/modelo/UnidadDeAlmacenamiento.java
+++ SupermarketHandler/src/global/modelo/UnidadDeAlmacenamiento.java
@@ -43,12 +43,11 @@ public class UnidadDeAlmacenamiento {
 	
 	//Métodos de manejo de productos disponibles en la unidad
 	public void agregarProducto(Codigo codigoProducto) {
-		productos.put(codigoProducto.getCodigo(), null);
+		if (productos.size()<capacidad) {productos.put(codigoProducto.getCodigo(), null);}
 	}
 	
 	public void eliminarProducto(Codigo codigoProducto) {
-		productos.remove(codigoProducto.getCodigo());
-		
+		if (productos.size()>0) {productos.remove(codigoProducto.getCodigo());}
 	}
 	
 
